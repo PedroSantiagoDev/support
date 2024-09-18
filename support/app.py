611f17260738ddb4304453548555eb49604e060a
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-09-18 16:02:02 -0300

    add fucntions

--- support/app.py
+++ support/app.py
@@ -1,5 +1,6 @@
 import pyperclip
 import pyautogui
+import webbrowser
 from random_location import set_locations
 from random_service import set_service
 from random_description import set_description
@@ -9,16 +10,30 @@ from time import sleep
 amount = int(input('Define the number of tickets to be opened: '))
 
 # Definir a localizacao de cada campo
-position_service = 1045,384
-position_applicant = 668,559
-position_email = 1322,592
-position_not_email = 1318,611
-position_location = 1039,895
-position_description = 859,635
-position_continue = 951,910
+position = 972,359
+position_service = 1034,411
+position_applicant = 715,564
+position_email = 1323,595
+position_not_email = 1315,618
+position_location = 1046,887
+position_description = 781,706
+position_continue = 950,956
+
+chrome_path = '/usr/bin/google-chrome %s'  # No Linux, altere o caminho se necessário
+
+url = 'https://glpi.codevasf.gov.br/front/ticket.form.php'
+
+webbrowser.get(chrome_path).open(url)
+
+# Categoriaglpi
+sleep(1)
+
+pyautogui.click(position, duration=1)
 
 for _ in range(amount):
-    # Categoria
+
+
+    # Categoriaglpi
     service = set_service()
     pyperclip.copy(service)
     pyautogui.click(position_service, duration=1)
@@ -39,8 +54,6 @@ for _ in range(amount):
     pyautogui.click(position_email, duration=1)
     # Marca como não
     pyautogui.click(position_not_email, duration=1)
-
-    pyautogui.click(940,703, duration=1)
 
     # # Localizacao
     location = set_locations()
